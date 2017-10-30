117b0f508ac41dd2d6b33faee5b114420e78b27d
nandinivalluri <nandiniv@s7inc.co>
2017-10-30 22:10:48 +0530

    done with radiobuttons

--- jvbhealthwellness/App/WorkoutInputs/Input1.js
+++ jvbhealthwellness/App/WorkoutInputs/Input1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { CheckBox, Button } from 'react-native-elements';
 
 export default class Input1 extends React.Component {
   render() {
@@ -8,6 +9,35 @@ export default class Input1 extends React.Component {
         <Text style={styles.text}>
         <Text>Did you Workout Today?</Text>
         </Text>
+        <View>
+          <CheckBox
+            center
+            title='Yes'
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
+            
+         /> 
+          <CheckBox
+            center
+            title='No'
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'         
+          /> 
+          <CheckBox
+            
+            title='Not Yet'
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
+          /> 
+
+        </View>
+        <View style={styles.footer}>
+          <Button
+            title='SKIP'
+          />  
+
+        </View>
+
       </View>
     );
   }
@@ -24,6 +54,12 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginTop: 250,
     marginLeft:15
+  },
+  footer: {
+    marginLeft: 10,
+    bottom: -10,
+
   }
+  
   
 });
