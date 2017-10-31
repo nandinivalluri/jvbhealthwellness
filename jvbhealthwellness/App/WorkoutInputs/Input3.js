14f93640faeba8987c38da5db91b5fc6ed7fa4b8
nandinivalluri <nandiniv@s7inc.co>
2017-10-31 21:44:42 +0530

    done with third page

--- jvbhealthwellness/App/WorkoutInputs/Input3.js
+++ jvbhealthwellness/App/WorkoutInputs/Input3.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
-
+import { CheckBox } from 'react-native-elements';
 
 export default class Input3 extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      checked: false,
+    };
+  }
   render() {
+    const { checked } = this.state;
+
     return (
       <View style={styles.container}>
         <Text style={styles.text}>
         <Text>Was Your Workout Today Enjoyable?</Text>
         </Text>
+        <View>
+          <CheckBox
+            center
+            title='Yes'
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o'
+            checked={this.state.checked}
+            onPress={() => this.setState({checked: !checked})}
+            
+         /> 
+          <CheckBox
+            center
+            title='No'
+            checkedIcon='dot-circle-o'
+            uncheckedIcon='circle-o' 
+            checked={this.state.checked}
+            onPress={() => this.setState({checked: !checked})}
+                    
+          /> 
+        </View>  
       </View>
     );
   }
