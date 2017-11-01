4882bfbd3f232ef7ac6e8981f6c393f50186894f
Normsoftware <nandinivalluri>
2017-11-01 18:34:43 +0530

    done with fourth page

--- jvbhealthwellness/App.js
+++ jvbhealthwellness/App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Input1 from './App/WorkoutInputs/Input1';
 import Input2 from './App/WorkoutInputs/Input2';
 import Input3 from './App/WorkoutInputs/Input3';
+import Input4 from './App/WorkoutInputs/Input4';
+import Input5 from './App/WorkoutInputs/Input5';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Input3 />
+        <Input5 />
       </View>
     );
   }
